14729447b623f78d717a721a1c6884d796e2198f
Lessllie Real <rlessllie@yahoo.com>
2019-10-12 04:52:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+
+def endbatch_badge_creator
+  name=[Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+end 
+
+def assign_rooms(name, room_number)
+  
+  "Hello, #{name}! You'll be assigned to room #{room_number}!"
+end 
